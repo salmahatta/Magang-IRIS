9892a77ed6233d7b9f72aa1d3c18386a419ede17
salmahatta <80839935+salmahatta@users.noreply.github.com>
2021-04-25 03:32:20 +0700

    Update latihan4.cpp

--- OOP/Latihan/latihan4.cpp
+++ OOP/Latihan/latihan4.cpp
@@ -1,7 +1,8 @@
-//Problem : Perform addition operation on complex data using class and object. The program should ask for real and imaginary part of two complex numbers, and display the real and imaginary parts of their sum.
-//Solution :
+//Perform addition operation on complex data using class and object. The program should ask for real and imaginary part of two complex numbers, and display the real and imaginary parts of their sum.
+
 
 #include <iostream>
+
 using namespace std;
 
 class Imagin{
