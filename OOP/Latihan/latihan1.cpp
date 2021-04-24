d6193ace11731ac82a20374a3606bc80530ba067
salmahatta <80839935+salmahatta@users.noreply.github.com>
2021-04-25 02:53:32 +0700

    Create latihan1.cpp

---
+++ OOP/Latihan/latihan1.cpp
@@ -0,0 +1,32 @@
+Problem : Write a class having two private variables and one member function which will return the area of the rectangle.
+Solution :
+
+using namespace std;
+
+class Rectangle 
+{
+ private:
+    int length;
+    int width;
+
+ public:
+    void panjanglebar(int x, int y)
+    {
+        length = x;
+        width = y;
+    }
+
+    int Area()
+    {
+        return width*length;
+    }
+};
+
+int main()
+{
+Rectangle rect;
+  rect.panjanglebar(3,4);
+  cout <<rect.Area()<<endl;
+
+    return 0;
+}
